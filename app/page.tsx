99ecc7aa64fc5ae949b078976e4e542d077e8409
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2024-11-03 15:21:01 +1100

    initialised app and naughts and crosses grid

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-neutral-400 size-64"></div>
+        <div className="bg-neutral-400 size-64"></div>
+        <div className="bg-neutral-400 size-64"></div>
+        <div className="bg-neutral-400 size-64"></div>
+        <div className="bg-neutral-400 size-64"></div>
+        <div className="bg-neutral-400 size-64"></div>
+        <div className="bg-neutral-400 size-64"></div>
+        <div className="bg-neutral-400 size-64"></div>
+        <div className="bg-neutral-400 size-64"></div>
+      </div>      
+    </div>
+  );
+}
